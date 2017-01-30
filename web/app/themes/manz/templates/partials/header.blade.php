13a60efff2b4008abe681ea880b9abc1dade7190
John Costallos <j.costallos@ubreakifix.com>
2017-01-30 14:34:42 -0500

    Modify header layout to align menu to the right.

--- web/app/themes/manz/templates/partials/header.blade.php
+++ web/app/themes/manz/templates/partials/header.blade.php
@@ -1,8 +1,8 @@
 <header class="banner p-1 mb-3">
   <div class="container">
-    <a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
-    <nav class="nav-primary">
-      @if (has_nav_menu('primary_navigation'))
+    <nav class="nav-primary navbar navbar-toggleable-md  p-0">
+      <a class="brand mr-auto" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
+    @if (has_nav_menu('primary_navigation'))
         {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
       @endif
     </nav>
