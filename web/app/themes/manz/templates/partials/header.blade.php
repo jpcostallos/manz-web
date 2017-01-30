cf71e3a2ba8e2bcc7ec34e0e9700c0dbeb02d111
John Costallos <j.costallos@ubreakifix.com>
2017-01-30 14:57:59 -0500

    Header fun

--- web/app/themes/manz/templates/partials/header.blade.php
+++ web/app/themes/manz/templates/partials/header.blade.php
@@ -1,7 +1,7 @@
-<header class="banner p-1 mb-3">
+<header class="banner p-1 mb-2">
   <div class="container">
     <nav class="nav-primary navbar navbar-toggleable-md  p-0">
-      <a class="brand mr-auto" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
+      <a class="brand mr-auto py-1" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
     @if (has_nav_menu('primary_navigation'))
         {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
       @endif
