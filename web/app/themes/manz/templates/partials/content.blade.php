9e899b8106c952dc3cff601480224058186464f4
John Costallos <j.costallos@ubreakifix.com>
2017-01-30 14:33:46 -0500

    Adjust padding areas on main page, add colors, make article area clickable instead of just title.

--- web/app/themes/manz/templates/partials/content.blade.php
+++ web/app/themes/manz/templates/partials/content.blade.php
@@ -1,9 +1,11 @@
-<article @php(post_class())>
-  <header>
-    <h2 class="entry-title"><a href="{{ get_permalink() }}">{{ get_the_title() }}</a></h2>
-    @include('partials/entry-meta')
-  </header>
-  <div class="entry-summary">
-    @php(the_excerpt())
-  </div>
+  <article @php(post_class('p-2'))>
+    <a href="{{ get_permalink() }}">
+      <header>
+      <h2 class="entry-title mt-0">{{ get_the_title() }}</h2>
+      @include('partials/entry-meta')
+    </header>
+    <div class="entry-summary">
+      @php(the_excerpt())
+    </div>
+  </a>
 </article>
--- web/app/themes/manz/templates/partials/page-header.blade.php
+++ web/app/themes/manz/templates/partials/page-header.blade.php
@@ -1,3 +1,3 @@
-<div class="page-header">
+<div class="page-header p-2">
   <h1>{!! App\title() !!}</h1>
 </div>
