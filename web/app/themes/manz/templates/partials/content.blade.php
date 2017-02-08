466758cbc1359c7537d11b291eb5a8e54342a6c3
John Costallos <j.costallos@ubreakifix.com>
2017-02-08 17:32:12 -0500

    Add featured image to home list

--- web/app/themes/manz/templates/partials/content.blade.php
+++ web/app/themes/manz/templates/partials/content.blade.php
@@ -1,11 +1,18 @@
-  <article @php(post_class('p-2'))>
+<article @php(post_class())>
     <a href="{{ get_permalink() }}">
-      <header>
-      <h2 class="entry-title mt-0">{{ get_the_title() }}</h2>
-      @include('partials/entry-meta')
-    </header>
-    <div class="entry-summary">
-      @php(the_excerpt())
-    </div>
-  </a>
+        <div class="row m-0">
+            <div class="col-md-8">
+                <header class="pt-1">
+                    <h2 class="entry-title mt-0">{{ get_the_title() }}</h2>
+                    @include('partials/entry-meta')
+                </header>
+                <div class="entry-summary">
+                    @php(the_excerpt())
+                </div>
+            </div>
+            <div class="col-md-4 pr-0">
+                <img class="img-fluid" src="{{the_post_thumbnail_url()}}" alt="">
+            </div>
+        </div>
+    </a>
 </article>
