03f35eb873cbea22186f892d690cbc40d7aab70e
John Costallos <j.costallos@ubreakifix.com>
2017-02-10 10:51:46 -0500

    Make links in article work properly.

--- web/app/themes/manz/templates/partials/content.blade.php
+++ web/app/themes/manz/templates/partials/content.blade.php
@@ -1,18 +1,22 @@
-<article @php(post_class())>
-    <a href="{{ get_permalink() }}">
-        <div class="row m-0">
-            <div class="col-md-8">
-                <header class="pt-1">
+<article @php(post_class('p-2 relative'))>
+    <div class="row">
+        @if(has_post_thumbnail())
+        <div class="col-md-4">
+            <a href="{{ get_permalink() }}">
+                <img class="img-thumbnail" src="{{the_post_thumbnail_url()}}" alt="">
+            </a>
+        </div>
+        @endif
+        <div class="@if(has_post_thumbnail()) col-md-8 @else col-md-12 @endif">
+            <a href="{{ get_permalink() }}">
+                <header>
                     <h2 class="entry-title mt-0">{{ get_the_title() }}</h2>
                     @include('partials/entry-meta')
                 </header>
                 <div class="entry-summary">
                     @php(the_excerpt())
                 </div>
-            </div>
-            <div class="col-md-4 pr-0">
-                <img class="img-fluid" src="{{the_post_thumbnail_url()}}" alt="">
-            </div>
+            </a>
         </div>
-    </a>
+    </div>
 </article>
